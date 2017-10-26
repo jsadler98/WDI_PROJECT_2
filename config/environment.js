1317375e462a6ac33dbc437700c8c7d8f44432d2
jsadler98 <joshy.sadler@btinternet.com>
2017-10-26 09:47:52 +0100

    deleted json lock

--- config/environment.js
+++ config/environment.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  dbUri: process.env.MONGODB_URI || 'mongodb://localhost/Game-Post',
+  dbUri: process.env.MONGODB_URI || 'mongodb://localhost/GamePost',
   sessionSecret: process.env.SESSION_SECRET || 'YghT5s617/1{%sDt'
 };
